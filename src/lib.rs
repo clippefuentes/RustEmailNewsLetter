6c2ad335ff64ced26c64630bb367c1862f361442
Clyne Fuentes <clippefuentes@gmail.com>
2022-11-19 19:12:24 -0600

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use std::net::TcpListener;
+use actix_web::{
+  web, App, HttpResponse, HttpServer
+};
+use actix_web::dev::Server;
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+  email: String,
+  name: String
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+pub struct Form<T>(pub T);
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+  HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+  let server = HttpServer::new(|| {
+    App::new()
+      .route("/health_check", web::get().to(health_check))
+      .route("/subscriptions", web::post().to(subscribe))
+  })
+      .listen(listener)?
+      .run();
+    Ok(server)
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use RustEmailNewsLetter::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    run(listener)?.await
+}
